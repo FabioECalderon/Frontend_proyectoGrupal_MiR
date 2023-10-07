e02125be9140a89e45820db83c16b3e81bd094bd
FabioECalderon <124937551+FabioECalderon@users.noreply.github.com>
2023-10-07 10:58:22 -0500

    E2e testing (#6)
    * Home and login e2e tests
    
    * Login e2e refactor

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,28 @@
 describe('template spec', () => {
+  beforeEach(function () {
+    cy.fixture('user.json').as('user');
+  });
+
+  it.only('Passes with correct credentials', function () {
+    cy.visit('/login');
+
+    cy.get('input[name="email"]').type(this.user.email);
+    cy.get('input[name="password"]').type(this.user.password);
+
+    cy.get('button[type="submit"]').click();
+
+    cy.contains(`Hola ${this.user.name}`);
+  });
+
+  it('Fails with incorrect credentials', function () {
+    cy.visit('/login');
+
+    cy.get('input[name="email"]').type(this.user.email);
+    cy.get('input[name="password"]').type(1234567);
+
+    cy.get('button[type="submit"]').click();
+
+    cy.contains(`invalid email or password`);
   it('passes', () => {
     cy.fixture('user.json').then((user) => {
       cy.visit('/login');
