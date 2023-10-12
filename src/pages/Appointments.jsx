98e15f55b2ec7e1ada46a26011f5f8e419ff43c0
Andres <marulandaandres19@gmail.com>
2023-10-11 20:40:18 -0500

    Add mercadopago connected to backend

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Container } from "react-bootstrap"
+import RedirectionButton from "../components/ButtonPay"
 
 export default function Appointments() {
   return (
-    <div>Appointments</div>
+    <>
+    <div className="d-flex justify-content-center"> 
+      <h1>Appointments</h1>
+    </div>
+    <div className="d-flex justify-content-center">
+      <RedirectionButton></RedirectionButton>   
+    </div>
+      
+    </>
+    
+    
   )
 }
