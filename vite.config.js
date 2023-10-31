c59f8ccf316d1885100909149f821f2e557d0f24
Andres Builes <119090182+AndrewSnake961@users.noreply.github.com>
2023-10-31 16:30:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.CI ? 5180 : 5173,},// Use a different port in CI environment
   plugins: [react()],
 })
